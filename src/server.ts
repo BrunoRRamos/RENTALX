da879e8e6742a9acaaab2757170ca0a71c996d13
MisterBeamCs <103748132+BrunoRRamos@users.noreply.github.com>
2023-03-05 16:20:22 -0300

    feat: routes/categoriesRoute

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from "express";
 
+import { categorieRoutes } from "./routes/categories.routes";
+
 const app = express();
 const port = 3333;
 
+app.use(express.json());
+app.use(categorieRoutes);
 app.listen(port, () => console.log(`listening on port: localhost:${port}`));
-
-app.get("/", (req, res) => {
-    res.json({ message: "batata" });
-});
